9ce87445ecaa416dea0514aebfbd18bd43036cd3
pechera <romankotliar7@gmail.com>
2022-05-21 04:39:46 +0300

    react app firs v

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import CostItem from "./components/CostItem";
+import Costs from "./components/Costs/Costs";
 
-function App() {
+const App = () => {
+  const costs = [
+    {
+      date: new Date(2021, 11, 2),
+      description: "Холодильник",
+      amount: 999.99,
+    },
+    {
+      date: new Date(2021, 2, 22),
+      description: "MacBook",
+      amount: 1999.99,
+    },
+    {
+      date: new Date(2021, 1, 2),
+      description: "Джинсы",
+      amount: 49.99,
+    },
+  ];
   return (
     <div>
-      <h1> Hello world!</h1>
-      <CostItem></CostItem>
+      <h1>My great React App</h1>
+      <Costs costs={costs} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/CostItem.js
+++
@@ -1,5 +0,0 @@
-function CostItem() {
-  return <h1>Касмотмный элемент</h1>;
-}
-
-export default CostItem;
